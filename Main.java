6fcf0692ebfa974f44c7308f6e514ca9dede5c3b
Admin <prejuvenile2@gmail.com>
2026-04-12 15:53:08 +0530

    EOS--12/04--EOD

--- Main.java
+++ Main.java
@@ -1,25 +1,2 @@
-interface I1{
-    default void show(){
-        System.out.println("One");
-    }
-}
+//Final
 
-interface I2{
-    default void show(){
-        System.out.println("Two");
-    }
-}
-
-class Caller implements I1,I2{
-    public void show(){
-        I1.super.show();
-    }
-
-    public static void main(String[] args) {
-
-    }
-}
-
-/** Interface Diamond Problem--*
- * When two methods come and compiler dont know which to call then we use I1.super.show();
- */
