c7c9d3dd3dcf81a6b7856d8b7385399cee7e45a5
Admin <prejuvenile2@gmail.com>
2026-04-12 14:30:33 +0530

    Interface2

--- Main.java
+++ Main.java
@@ -1,13 +1,49 @@
-//Interface
+//INTERFACE
+/*
+class Ex{
+    void fun(){
+        System.out.println("Hello .. Students are good");
+    }
+}
 
 interface i1{
-    static void msg(){
-        System.out.println("dslkfsk");
+    void fun1();
+}
+
+class Caller extends Ex implements i1{
+    public void fun1(){
+
+    }
+}*/
+
+interface i1{
+    default void fun(){
+        System.out.println("slckslk");
+    }
+    void fun1();
+    void fun2();
+}
+
+class caller implements i1{
+    public void fun1(){
+
+    }
+    public void fun2(){
+
     }
 }
 
-class Caller{
-    public static void main(String[] args) {
-        i1.msg();
-    }
+public static void main(String[] args) {
+
 }
+
+
+
+
+
+
+
+
+
+
+
