cf56cdfbd4f2ccd2c78fe21a9df75881677da2a8
Admin <prejuvenile2@gmail.com>
2026-04-12 09:58:48 +0530

    WrapperIntegerUnbox

--- Main.java
+++ Main.java
@@ -1,11 +1,9 @@
 //Wrapper classes
-//Primitive --> Object
+
 class Caller{
     public static void main(String[] args) {
-        int a=10;
-        Integer b = a; //auto Boxing
-        Integer c =11;
-        System.out.println(b.getClass());
-        System.out.print(c);
+        //Auto Unboxing
+        Integer a =10;
+        int b=a;
     }
 }
