c4f81b39709089f9b8746575a54652d2df09d129
Admin <prejuvenile2@gmail.com>
2026-04-18 14:37:59 +0530

    Problem

--- Main.java
+++ Main.java
@@ -1,31 +1,29 @@
-//Collection FRAMEWORKS
-//LinkedList
+//problem
 
-import java.util.Iterator;
 import java.util.LinkedList;
 
 class call{
     public static void main(String[] args) {
-        LinkedList ll = new LinkedList();
-        ll.add("Krushna");
+        LinkedList<Integer> ll = new LinkedList<Integer>();
         ll.add(10);
-        ll.add(13.23);
-        ll.add(13.24f);
-        ll.add(null);
+        ll.add(11);
+        ll.add(8);
+        ll.add(7);
+        ll.add(5);
+        ll.add(1);
         System.out.println(ll);
 
-        //with looping
-        for(int i=0;i<ll.size();i++){
-            System.out.println(ll.get(i));
-        }
-        //with for each
-        for(Object o:ll){
-            System.out.println(o);
-        }
-        //with iterator
-        Iterator<Object> it = ll.iterator();
-        while(it.hasNext()){
-            System.out.println(it.next());
-        }
+//        for(int i=0;i<ll.size();i++){
+//            int it = ll.get(i);
+//            if(it%2==0){
+//                System.out.println("Even " +it );
+//            }
+//            else {
+//                System.out.println("Odd "+it);
+//            }
+//        }
+        ll.removeIf(x->x%2==0);
+        System.out.println(ll);
+
     }
 }
