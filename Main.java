298bdaecbd8224f0a2b1326607d42d5bafe6c6f2
Admin <prejuvenile2@gmail.com>
2026-04-18 13:25:34 +0530

    StingBuffer-Object class

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
-//sTIRNG ARE IMMUTABLE
+//Object class String class
 
-class String1{
+class Stirng1{
     public static void main(String[] args) {
-        String s = new String("Pratap");
-        s= s.concat("socfteare");
-        System.out.println(s);
-
+        StringBuffer sb1 = new StringBuffer("pratap");
+        StringBuffer sb2 = new StringBuffer("pratap");
+        System.out.println(sb1==sb2);
+        System.out.println(sb1.equals(sb2)); //this calls Object class .equals because it doesnt override
     }
 }
