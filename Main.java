4fb82a7220f02f9f0a2131b098fdcae93aa49f26
Admin <prejuvenile2@gmail.com>
2026-04-18 11:50:53 +0530

    Sting are immutable

--- Main.java
+++ Main.java
@@ -1,25 +1,10 @@
-//write a java prohgramm to count uppercase and lower case letter from given string
+//sTIRNG ARE IMMUTABLE
 
+class String1{
+    public static void main(String[] args) {
+        String s = new String("Pratap");
+        s= s.concat("socfteare");
+        System.out.println(s);
 
-
-class counter{
-    public void count(String s){
-        int countlo=0;
-        int countup=0;
-        for(int i=0;i<s.length();i++){
-            Character ch = s.charAt(i);
-            if(Character.isLowerCase(ch)){
-                countlo++;
-            }
-            if(Character.isUpperCase(ch)){
-                countup++;
-            }
-        }
-        System.out.println(countlo);
-        System.out.println(countup);
-    }
-    public static void main(String[] args){
-        counter c = new counter();
-        c.count("JSKjdjks");
     }
 }
