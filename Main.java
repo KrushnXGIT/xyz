bb34fce3c16c4977e806ce3082a1ad169a6160e5
Admin <prejuvenile2@gmail.com>
2026-04-18 16:01:41 +0530

    TreeSet, EOS--12/04--EOD

--- Main.java
+++ Main.java
@@ -1,24 +1,17 @@
 //Collection FRAMEWORKS
-//HashSet
-//LinkedHashSet just changed HashSet <->LinkedHashSet
+//TreeSet
 
-
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedHashSet;
+import java.util.TreeSet;
 
 class call{
     public static void main(String[] args) {
-        LinkedHashSet hs = new LinkedHashSet();
-        hs.add("jello");
-        hs.add("helo");
-        hs.add(10);
-        hs.add(10.21);
-        hs.add(12.32f);
-        hs.add(null);
-        System.out.println(hs);
-        for(int i=0;i<hs.size();i++){
-           // System.out.println(hs.get(i));//this is problem here
+        TreeSet ts = new TreeSet();
+        ts.add("Hello");
+        ts.add("Jello");
+        ts.add("Command");
+        System.out.println(ts);
+        for(int i=0;i<ts.size();i++){
+            //System.out.println(ts.get(i));//not allowed;
         }
     }
 }
