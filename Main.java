fce618de34df8bb40d81c739f5467742fe026e19
Admin <prejuvenile2@gmail.com>
2026-04-18 09:53:34 +0530

    Session Start 18/04 --AutoPromotion

--- Main.java
+++ Main.java
@@ -1,2 +1,20 @@
-//Final
+//Automatic Promotion
+
+class Test{
+    void m1(int i){
+        System.out.println("Int");
+    }
+    void m1(float k){
+        System.out.println("Float");
+    }
+
+    public static void main(String[] args) {
+        Test t = new Test();
+        t.m1(1);
+        t.m1(5.2f);
+        t.m1('c');
+        //t.m1(10.6); //<-this is problematic because double has no auto promotion
+
+    }
+}
 
