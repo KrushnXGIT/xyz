258222f54906b2b12e3b080c0877f3673be051c9
Admin <prejuvenile2@gmail.com>
2026-04-18 10:43:43 +0530

    String , Var-arg method ... ,

--- Main.java
+++ Main.java
@@ -1,20 +1,27 @@
-//Automatic Promotion
+//String SCP HEAP , OBJECT->STRING STRINGBUFFER STRINGBUILDER
 
-class Test{
-    void m1(int i){
-        System.out.println("Int");
-    }
-    void m1(float k){
-        System.out.println("Float");
-    }
 
+class Call{
+    public void m1(String s){
+        System.out.println("String versioned");
+    }
+    public void m1(StringBuffer s){
+        System.out.println("StringBuffer versioned");
+    }
+    public void m2(int i){
+        System.out.println("Gen Method");
+    }
+    public void m2(int ...i){
+        System.out.println("VAR-ARG MEHTOD");
+    }
     public static void main(String[] args) {
-        Test t = new Test();
-        t.m1(1);
-        t.m1(5.2f);
-        t.m1('c');
-        //t.m1(10.6); //<-this is problematic because double has no auto promotion
-
+        Call c = new Call();
+        c.m1("Pratap");
+        c.m1(new StringBuffer("KDK"));
+        c.m2();
+        c.m2(10,20);
     }
+
 }
+
 
