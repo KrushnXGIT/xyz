1a12c2303ab71db236b267811bc8c7e01ed6bc59
Admin <prejuvenile2@gmail.com>
2026-04-19 09:48:31 +0530

    TreeSet--, implements Comparator

--- Main.java
+++ Main.java
@@ -1,11 +1,33 @@
-//program SOD
-import java.util.*;
-class Anagram{
-    static void anag(String s1,String s2){
-        if(s1.length()!=s2.length()){
-            System.out.println("not an anagram");
-        }else{
-            s1 = s1.sort()
-        }
+//Collection FRAMEWORKS
+//TreeSet
+
+import java.util.Comparator;
+import java.util.TreeSet;
+
+class Mycomparator implements Comparator<Integer>{
+//    @Override
+//    public Comparator<Integer> reversed() {
+//        return Comparator.super.reversed();
+//    }
+    @Override
+    public int compare(Integer o1,Integer o2){
+        Integer a = (Integer) o1;
+        Integer b = (Integer) o2;
+        return -a.compareTo(b);
+        //return a.compareTo(b);<--this will natural sort
+    }
+}
+
+class Tree{
+    public static void main(String[] args) {
+        TreeSet<Integer> t = new TreeSet<Integer>(new Mycomparator());
+        t.add(10);
+        t.add(12);
+        t.add(1);
+        t.add(-2);
+        t.add(-13);
+        t.add(-1);
+        t.add(7);
+        System.out.println(t);
     }
 }
