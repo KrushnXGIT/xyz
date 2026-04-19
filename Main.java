fc37dcec3adbf8aa4dc9e71c5543735605509dc1
Admin <prejuvenile2@gmail.com>
2026-04-19 14:20:30 +0530

    OptionalPractice

--- Main.java
+++ Main.java
@@ -1,17 +1,31 @@
-interface Krushna{
-    default void method1(){
-        System.out.println("DefAULT METHOD");
-    }
-    static void method2(){
-        System.out.println("sTATIC METHOD");
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+class opti{
+    public static Optional<String> getName(){
+        String s="mit";
+        return Optional.ofNullable(s);
     }
 }
 
-class call implements Krushna{
+class OptionalPractice{
     public static void main(String[] args) {
-        call l = new call();
-        l.method1();
-        Krushna.method2();
+        String s=null;
+        /*
+        if(s==null) System.out.println("this is null");
+        else System.out.println(s.length()); //i cannot do if-else if i have 1000strings this will make code less readeable
+        //therefore we can use OptionalCLASS
+        */
+        System.out.println();
 
+        Optional<String> opt= Optional.ofNullable(s);
+        System.out.println(opt.isPresent());
+        //System.out.println(opt.get());//this fails if s is null
+        System.out.println(opt.orElse("string is null"));
+
+
+        Optional<String> newOPT = opti.getName();
+        System.out.println(newOPT.orElse("String empty"));
     }
+
 }
