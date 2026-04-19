733a465e7109dec2fdfc11a49adc3a64135861cc
Admin <prejuvenile2@gmail.com>
2026-04-19 10:56:57 +0530

    Lambda Function

--- Main.java
+++ Main.java
@@ -1,12 +1,24 @@
-//Map WORKS
-//TreeMap
+//JAVA--8--FEATURES
+//FEATURE-LAMBDA FUNCTION
+
+@FunctionalInterface
+interface addInt {
+    int sum(int a,int b);
+}
+
+interface String_{
+    int lenst(String s);
+}
 
 
-import java.util.TreeMap;
 
 class call{
     public static void main(String[] args) {
-        TreeMap t = new TreeMap();
-        t.put()
+//        Runnable hello = () -> System.out.println("hello");
+//        System.out.println(hello); //outputs=> call$$Lambda$14/0x0000023047001200@e9e54c2
+        addInt add = (p,q)->p+q;
+        System.out.println(add.sum(10,12));
+        String_ fun = (s)->s.length();
+        System.out.println(fun.lenst("Krushna"));
     }
 }
